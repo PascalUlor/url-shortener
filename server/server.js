3e3224cd894787484c955df4d072e71a65b22281
PascalUlor <pascalulor@yahoo.com>
2020-06-23 19:13:13 +0100

    serve frontend from server

--- server/server.js
+++ server/server.js
@@ -34,6 +34,13 @@ app.use(function(req, res, next) {
   }
 });
 
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+
+
 app.use('/api', routes);
 
 module.exports = app;

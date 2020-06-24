8dfb4e1eb52605107dd377525e685410b703011b
PascalUlor <pascalulor@yahoo.com>
2020-06-24 10:51:08 +0100

    add nginx configs

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const fs = require('fs');
 const app = require('./server/server');
 
 const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => {
-    console.log(`Application started  on port`, PORT);
-   });
+    if (process.env.DYNO) {
+        console.log('Running on Heroku...');
+        fs.openSync('/tmp/app-initialized', 'w');
+      }
+    console.log('Node app is running on port:', PORT);
+});

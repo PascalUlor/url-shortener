fa12c009c81b4556e896fa18d30c3b59549f1d8d
PascalUlor <pascalulor@yahoo.com>
2020-06-24 13:10:46 +0100

    remove heroku post build

--- server/server.js
+++ server/server.js
@@ -34,13 +34,6 @@ app.use(function(req, res, next) {
   }
 });
 
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-
-
 app.use('/api', routes);
 
 module.exports = app;

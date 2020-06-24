f7f7ea6f1905e86028fa325aa2d13d8bbd496806
PascalUlor <pascalulor@yahoo.com>
2020-06-24 13:02:17 +0100

    run servers concurrently

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = require('./server/server');
 const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => {
-    // if (process.env.DYNO) {
-    //     console.log('Running on Heroku...');
-    //     fs.openSync('/tmp/app-initialized', 'w');
-    //   }
+    if (process.env.DYNO) {
+        console.log('Running on Heroku...');
+        fs.openSync('/tmp/app-initialized', 'w');
+      }
     console.log('Node app is running on port:', PORT);
 });
